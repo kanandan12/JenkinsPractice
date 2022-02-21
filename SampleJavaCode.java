eca5f12f288a3d38d6e1aae77d247f88bd756091
Kamalakannan Anandan <39454435+kanandan12@users.noreply.github.com>
2022-02-21 15:48:03 -0600

    Initial Commit

---
+++ SampleJavaCode.java
@@ -0,0 +1,13 @@
+/**
+ * Jenkins Practice Sample Java Program
+ *
+ */
+public class SampleJavaCode 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is sample Jenkins Practice" );
+        System.out.println( "java Program" );
+              
+    }
+}
